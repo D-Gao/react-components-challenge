292fec30c3e17ca9142c0faa95f2d527d070c64d
D-Gao <huang245415359@gmail.com>
2024-11-10 22:00:15 +0100

    chore: added theme of shancn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="h-screen w-screen flex justify-center items-center bg-background">
+        <h1 className="text-3xl font-bold underline text-primary">
+          Hello world!
+        </h1>
       </div>
     </>
   );
