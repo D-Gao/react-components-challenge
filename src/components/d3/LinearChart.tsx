58458f6a826156a4b97880bb0dedc7f0b2b77fce
D-Gao <huang245415359@gmail.com>
2024-12-26 22:04:32 +0100

    feat: solid & dashed linear chart

--- src/components/d3/LinearChart.tsx
+++ src/components/d3/LinearChart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
@@ -20,6 +21,9 @@ const ZoomableAreaChart: React.FC<ZoomableAreaChartProps> = ({ data }) => {
       date: new Date(d.date),
       value: d.value,
     }));
+    const breakpoint = new Date("2001-09-01");
+    const normalData = parsedData.filter((d) => d.date <= breakpoint);
+    const dashedData = parsedData.filter((d) => d.date >= breakpoint);
 
     // Chart dimensions
     const width = 928;
@@ -42,12 +46,19 @@ const ZoomableAreaChart: React.FC<ZoomableAreaChartProps> = ({ data }) => {
       .range([height - marginBottom, marginTop]);
 
     // Area generator
-    const areaGenerator = d3
+    /* const areaGenerator = d3
       .area<{ date: Date; value: number }>()
       .curve(d3.curveStepAfter)
       .x((d) => x(d.date)!)
       .y0(y(0))
-      .y1((d) => y(d.value));
+      .y1((d) => y(d.value)); */
+
+    // Linear generator
+    const lineGenerator = d3
+      .line<{ date: Date; value: number }>()
+      .curve(d3.curveBasis)
+      .x((d) => x(d.date)!)
+      .y((d) => y(d.value));
 
     // SVG setup
     const svg = d3
@@ -58,7 +69,7 @@ const ZoomableAreaChart: React.FC<ZoomableAreaChartProps> = ({ data }) => {
       .attr("style", "max-width: 100%; height: auto;");
 
     // Clip path
-    const clipId = `clip-${Math.random().toString(36).substr(2, 9)}`;
+    const clipId = `clip-linear`;
     svg
       .append("clipPath")
       .attr("id", clipId)
@@ -69,12 +80,57 @@ const ZoomableAreaChart: React.FC<ZoomableAreaChartProps> = ({ data }) => {
       .attr("height", height - marginTop - marginBottom);
 
     // Area
-    const path = svg
+    /* const path = svg
       .append("path")
       .datum(parsedData)
       .attr("clip-path", `url(#${clipId})`)
       .attr("fill", "steelblue")
-      .attr("d", areaGenerator);
+      .attr("d", areaGenerator); */
+
+    /* const path = svg
+      .append("path")
+      .datum(parsedData)
+      .attr("clip-path", `url(#${clipId})`)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", areaGenerator); */
+
+    // Normal line
+    const pathNormal = svg
+      .append("path")
+      .datum(normalData)
+      .attr("clip-path", `url(#${clipId})`)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", lineGenerator);
+
+    // Dashed line
+    const pathDashed = svg
+      .append("path")
+      .datum(dashedData)
+      .attr("clip-path", `url(#${clipId})`)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-dasharray", "5,5")
+      .attr("d", lineGenerator);
+    /* .call((path) => {
+        const length = path.node()?.getTotalLength() ?? 0;
+        path
+          .attr("stroke-dasharray", `${length},${length}`)
+          .attr("stroke-dashoffset", length);
+      }); // Set initial dash state */
+
+    const pathTotal = svg
+      .append("path")
+      .datum(parsedData)
+      .attr("clip-path", `url(#${clipId})`)
+      .attr("fill", "none")
+      .attr("stroke", "black")
+      .attr("stroke-width", 1.5)
+      .attr("d", lineGenerator);
 
     // Axes
     const xAxis = (
@@ -108,7 +164,7 @@ const ZoomableAreaChart: React.FC<ZoomableAreaChartProps> = ({ data }) => {
     // Zoom behavior
     const zoom = d3
       .zoom<SVGSVGElement, unknown>()
-      .scaleExtent([1, 32])
+      .scaleExtent([1, 512])
       .extent([
         [marginLeft, 0],
         [width - marginRight, height],
@@ -119,12 +175,50 @@ const ZoomableAreaChart: React.FC<ZoomableAreaChartProps> = ({ data }) => {
       ])
       .on("zoom", (event) => {
         const xz = event.transform.rescaleX(x);
-        path.attr(
+        /* path.attr(
           "d",
           areaGenerator.x((d) => xz(d.date)!)
+        ); */
+        pathNormal.attr(
+          "d",
+          lineGenerator.x((d) => xz(d.date)!)
         );
+        pathDashed.attr(
+          "d",
+          lineGenerator.x((d) => xz(d.date)!)
+        );
+
+        pathTotal.attr(
+          "d",
+          lineGenerator.x((d) => xz(d.date)!)
+        );
+        /* svg
+          .selectAll("path")
+          .attr("d", (d, i) =>
+            i === 0
+              ? lineGenerator.x((d) => xz(d.date)!)
+              : lineGenerator.x((d) => xz(d.date)!)
+          ); */
         gx.call(xAxis, xz);
       });
+
+    // Initial animation
+
+    const animatePath = (
+      path: d3.Selection<SVGPathElement, any, null, undefined>
+    ) => {
+      const length = path.node()?.getTotalLength() ?? 0;
+      path
+        .attr("stroke-dasharray", `${length},${length}`)
+        .attr("stroke-dashoffset", 0)
+        .transition()
+        .duration(3000)
+        .ease(d3.easeCubicInOut)
+        .attr("stroke-dashoffset", -length)
+        .remove();
+    };
+
+    animatePath(pathTotal);
 
     svg
       .call(zoom)
